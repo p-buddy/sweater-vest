ff6513c382ea7f8ad50871355f1c0b83f35dfb01
p-buddy <parkermalachowsky@gmail.com>
2025-05-27 22:00:27 +0000

    trying to recreate issue

---
+++ test-consumer/src/routes/static/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Sweater } from "sweater-vest";
+</script>
+
+<Sweater body={async () => {}}>
+  {#snippet vest(pocket: {})}
+    <div class="w-full h-full bg-red-500">
+      <h1>Veset</h1>
+    </div>
+  {/snippet}
+</Sweater>
